9b15405acf20031baab41f8abc09b33398fb5752
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-06-29 15:26:17 +0530

    CSE- slider hero added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import AchievementList from "../components/achievement-list";
 import PopularCourseList from "../components/popular-course-list";
 import ExploreCourseList from "../components/explore-course-list";
+import HeroCarousel from "../components/hero-carousel";
 
 const Home = () => {
   return (
     <>
+      <HeroCarousel />
       <AchievementList />
       <PopularCourseList />
       <ExploreCourseList />
