847b0b0e310031bee6f8639da36779bdb1374e8e
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-08-01 19:34:00 +0530

    CSE:Slider Title Added

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -4,6 +4,7 @@ import slider2 from "../images/slider-2.jpg";
 import slider3 from "../images/slider-3.0.jpg";
 import slider4 from "../images/slider-4.jpg";
 import styled from "styled-components";
+import { mq } from "../styles/breakpoints";
 
 function HeroCarousel() {
   return (
@@ -11,35 +12,43 @@ function HeroCarousel() {
       <Carousel.Item>
         <Image src={slider1} alt="slider" />
         <Carousel.Caption>
-          <h3>First slider</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <SliderTitle>Welcome to Al-Huzaifi Academy</SliderTitle>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image src={slider2} alt="slider" />
         <Carousel.Caption>
-          <h3>Second slider</h3>
-          <p>
+          <SliderTitle>
+            Crafted to nurture souls and spread excellency
+          </SliderTitle>
+          {/* <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image src={slider3} alt="slider" />
         <Carousel.Caption>
-          <h3>Third slider</h3>
-          <p>
+          <SliderTitle>
+            Dedicated to provide you with the tools to explore the depth of
+            Islamic Science
+          </SliderTitle>
+          {/* <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <Image src={slider4} alt="slider" />
         <Carousel.Caption>
-          <h3>Fourth slider</h3>
-          <p>
+          <SliderTitle>
+            A guide to strengthen your connection with faith, empowering you to
+            lead a life a purpose
+          </SliderTitle>
+          {/* <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
@@ -51,4 +60,26 @@ export default HeroCarousel;
 const Image = styled.img`
   width: 100%;
   height: auto;
+`;
+
+const SliderTitle = styled.h3`
+  // color: ${(props) => props.theme.colors.activeBg};
+  color: #555d50;
+  font-family: "Oswald", sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+
+  ${mq("sm")} {
+    color: #555d50;
+    font-family: "Oswald", sans-serif;
+    font-size: 24px;
+    font-weight: 400;
+  }
+
+  ${mq("md")} {
+    font-size: 32px;
+    font-weight: 400;
+    color: #555d50;
+    font-family: "Oswald", sans-serif;
+  }
 `;
