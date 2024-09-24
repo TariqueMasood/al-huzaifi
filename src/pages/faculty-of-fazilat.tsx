303a71f8f61fdee9c5aaa4f5674616594b494e35
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-09-25 00:12:50 +0530

    CSE:Image added in fazilat page

--- src/pages/faculty-of-fazilat.tsx
+++ src/pages/faculty-of-fazilat.tsx
@@ -1,114 +1,125 @@
 import styled from "styled-components";
 import { mq } from "../styles/breakpoints";
 import { Container } from "react-bootstrap";
+import BgImage from "../../src/images/faculty-of-fazilat.png";
 
 export const FacultyOfFazilatPage = () => {
   return (
-    <Wrapper>
-      <Container>
-        <Title>Faculty of Fazilat</Title>
-        <SubTitle>Overview:</SubTitle>
-        <Content>
-          The Faculty of Fazilat at <BoldText>Al-Huzaifi Academy</BoldText>{" "}
-          offers advanced specialization programs designed to provide students
-          with in-depth knowledge and expertise in specific areas of Islamic
-          studies. Similar to graduate programs in universities, the Fazilat
-          programs are rigorous and comprehensive, aimed at developing scholars
-          who can contribute significantly to their fields of study. This is a
-          2-year specialization program, tailored for students who have already
-          completed foundational Islamic studies and wish to pursue higher
-          education in a focused area.
-        </Content>
-        <Space />
-        <Content>
-          The Faculty of Fazilat emphasizes critical thinking, research, and
-          scholarly excellence, preparing students to become experts and leaders
-          in their respective specializations. Our curriculum combines classical
-          Islamic scholarship with contemporary academic approaches, ensuring
-          that students receive a well-rounded and robust education.
-        </Content>
-        <SubTitle>Areas of Specialization:</SubTitle>
-        <Space />
-        <ol>
-          <List>Fazilat in Quranic Science</List>
+    <>
+      <div>
+        <Image src={BgImage} alt="pic"></Image>
+      </div>
+      <Wrapper>
+        <Container>
+          <Title>Faculty of Fazilat</Title>
+          <SubTitle>Overview:</SubTitle>
           <Content>
-            - This specialization focuses on advanced studies in Quranic
-            exegesis (Tafseer), the science of the Quran, and the various
-            methodologies used in interpreting the Quranic text. Students will
-            engage with classical and contemporary Tafseer works, gaining a deep
-            understanding of the Quran’s meanings and applications.
+            The Faculty of Fazilat at <BoldText>Al-Huzaifi Academy</BoldText>{" "}
+            offers advanced specialization programs designed to provide students
+            with in-depth knowledge and expertise in specific areas of Islamic
+            studies. Similar to graduate programs in universities, the Fazilat
+            programs are rigorous and comprehensive, aimed at developing
+            scholars who can contribute significantly to their fields of study.
+            This is a 2-year specialization program, tailored for students who
+            have already completed foundational Islamic studies and wish to
+            pursue higher education in a focused area.
           </Content>
-          <List>Fazilat in Hadith Science</List>
+          <Space />
           <Content>
-            - Students specializing in Hadith Science will delve into the
-            collection, authentication, and interpretation of Hadith. This
-            program covers the major Hadith compilations, the principles of
-            Hadith criticism, and the methodologies used by classical and modern
-            scholars to evaluate and interpret Prophetic traditions.
+            The Faculty of Fazilat emphasizes critical thinking, research, and
+            scholarly excellence, preparing students to become experts and
+            leaders in their respective specializations. Our curriculum combines
+            classical Islamic scholarship with contemporary academic approaches,
+            ensuring that students receive a well-rounded and robust education.
           </Content>
-          <List>Fazilat in Islamic Jurisprudence</List>
+          <SubTitle>Areas of Specialization:</SubTitle>
+          <Space />
+          <ol>
+            <List>Fazilat in Quranic Science</List>
+            <Content>
+              - This specialization focuses on advanced studies in Quranic
+              exegesis (Tafseer), the science of the Quran, and the various
+              methodologies used in interpreting the Quranic text. Students will
+              engage with classical and contemporary Tafseer works, gaining a
+              deep understanding of the Quran's meanings and applications.
+            </Content>
+            <List>Fazilat in Hadith Science</List>
+            <Content>
+              - Students specializing in Hadith Science will delve into the
+              collection, authentication, and interpretation of Hadith. This
+              program covers the major Hadith compilations, the principles of
+              Hadith criticism, and the methodologies used by classical and
+              modern scholars to evaluate and interpret Prophetic traditions.
+            </Content>
+            <List>Fazilat in Islamic Jurisprudence</List>
+            <Content>
+              - This specialization provides an in-depth study of Fiqh (Islamic
+              jurisprudence) and Usul al-Fiqh (principles of jurisprudence).
+              Students will explore the legal rulings of the four major Sunni
+              schools of thought, comparative Fiqh, and contemporary issues in
+              Islamic law, equipping them to address modern legal challenges
+              with an Islamic perspective.
+            </Content>
+            <List>Fazilat in Theology</List>
+            <Content>
+              - This program focuses on advanced theological studies, covering
+              the core beliefs of Islam, classical and modern theological
+              debates, and the development of Islamic thought. Students will
+              engage with key theological texts and explore the works of
+              prominent theologians to gain a comprehensive understanding of
+              Islamic creed (Aqidah).
+            </Content>
+            <List>Fazilat in Islamic Finance</List>
+            <Content>
+              {" "}
+              - This specialization focuses on the principles and practices of
+              Islamic finance and banking. Students will study Sharia-compliant
+              financial instruments, Islamic economic theory, and contemporary
+              issues in Islamic finance, preparing them for careers in the
+              growing field of Islamic banking and finance.
+            </Content>
+            <List>Fazilat in Arabic Language</List>
+            <Content>
+              {" "}
+              - Specializing in the Arabic language, this program offers
+              advanced studies in Arabic grammar, syntax, rhetoric, and
+              literature. Students will achieve proficiency in reading and
+              interpreting classical Arabic texts and gain an appreciation for
+              the rich literary heritage of the Arabic-speaking world.
+            </Content>
+          </ol>
+          <Space />
           <Content>
-            - This specialization provides an in-depth study of Fiqh (Islamic
-            jurisprudence) and Usul al-Fiqh (principles of jurisprudence).
-            Students will explore the legal rulings of the four major Sunni
-            schools of thought, comparative Fiqh, and contemporary issues in
-            Islamic law, equipping them to address modern legal challenges with
-            an Islamic perspective.
+            The Faculty of Fazilat is dedicated to fostering academic excellence
+            and scholarly achievement. Our specialized programs are designed to
+            cultivate experts who can contribute to the advancement of Islamic
+            knowledge and address the needs of the Muslim community in various
+            fields. Through rigorous study and research, we aim to produce
+            graduates who are well-equipped to uphold and promote the rich
+            tradition of Islamic scholarship.
           </Content>
-          <List>Fazilat in Theology</List>
-          <Content>
-            - This program focuses on advanced theological studies, covering the
-            core beliefs of Islam, classical and modern theological debates, and
-            the development of Islamic thought. Students will engage with key
-            theological texts and explore the works of prominent theologians to
-            gain a comprehensive understanding of Islamic creed (Aqidah).
-          </Content>
-          <List>Fazilat in Islamic Finance</List>
-          <Content>
-            {" "}
-            - This specialization focuses on the principles and practices of
-            Islamic finance and banking. Students will study Sharia-compliant
-            financial instruments, Islamic economic theory, and contemporary
-            issues in Islamic finance, preparing them for careers in the growing
-            field of Islamic banking and finance.
-          </Content>
-          <List>Fazilat in Arabic Language</List>
-          <Content>
-            {" "}
-            - Specializing in the Arabic language, this program offers advanced
-            studies in Arabic grammar, syntax, rhetoric, and literature.
-            Students will achieve proficiency in reading and interpreting
-            classical Arabic texts and gain an appreciation for the rich
-            literary heritage of the Arabic-speaking world.
-          </Content>
-        </ol>
-        <Space />
-        <Content>
-          The Faculty of Fazilat is dedicated to fostering academic excellence
-          and scholarly achievement. Our specialized programs are designed to
-          cultivate experts who can contribute to the advancement of Islamic
-          knowledge and address the needs of the Muslim community in various
-          fields. Through rigorous study and research, we aim to produce
-          graduates who are well-equipped to uphold and promote the rich
-          tradition of Islamic scholarship.
-        </Content>
-      </Container>
-    </Wrapper>
+        </Container>
+      </Wrapper>
+    </>
   );
 };
 
 const Wrapper = styled.div`
   max-width: 850px;
   width: 100%;
-  padding: 20px 0px;
+  // padding: 20px 0px;
   margin: auto;
 
   ${mq("sm")} {
     max-width: 850px;
     width: 100%;
-    padding: 40px 0px;
+    // padding: 40px 0px;
     margin: auto;
   }
+`;
+
+const Image = styled.img`
+  width: 100%;
 `;
 
 const Title = styled.h1`
