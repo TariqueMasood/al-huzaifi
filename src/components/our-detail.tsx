d157b30b4b41f598a2088cd93ef8a64e6b2641f4
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-11-29 00:22:15 +0530

    Home page done

--- src/components/our-detail.tsx
+++ src/components/our-detail.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import bgImg from "../../src/images/home-page-banner-img.png";
+import { mq } from "../styles/breakpoints";
 
 export const OurDetail = () => {
   return (
@@ -16,8 +17,8 @@ export const OurDetail = () => {
           passionate eduactors from around the world, we are committed to
           supporting each student's unique learnign path.
         </p>
+        <LearnMore>learn more</LearnMore>
       </ContentWrapper>
-      <LearnMore>learn more</LearnMore>
     </Wrapper>
   );
 };
@@ -31,44 +32,88 @@ const BgImage = styled.img`
   height: auto;
 `;
 
-const LearnMore = styled.button`
-  position: absolute;
-  bottom: 16%;
-  left: 16%;
-  text-transform: capitalize;
-  background-color: #fff;
-  border: 1px solid ${(props) => props.theme.colors.themeColor};
-  color: ${(props) => props.theme.colors.themeColor};
-  border-radius: 4px;
-  padding: 6px 24px;
-  font-size: 16px;
-  transition: all ease-in 0.4s;
-
-  &:hover {
-    background: ${(props) => props.theme.colors.themeColor};
-    color: #fff;
-  }
-`;
-
 const ContentWrapper = styled.div`
-  max-width: 850px;
   width: 100%;
   position: absolute;
-  top: 16%;
-  left: 16%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
   h2 {
-    font-size: 56px;
+    padding: 0 7px;
+    color: #cdad7e;
     font-style: italic;
-    font-weight: 600;
-    letter-spacing: 1px;
-    margin-bottom: 36px;
-    color: ${(props) => props.theme.colors.themeColor};
+    font-size: 14px;
+    letter-spacing: 0px;
+    margin-bottom: 0px;
+    font-weight: 500;
+
+    ${mq("sm")} {
+      font-size: 24px;
+      letter-spacing: 1px;
+      padding: 0 60px;
+      margin-bottom: 18px;
+      font-weight: 600;
+    }
+
+    ${mq("md")} {
+      padding: 0 100px;
+      font-size: 60px;
+      font-weight: 600;
+      letter-spacing: 1px;
+      margin-bottom: 36px;
+    }
   }
 
   p {
-    font-size: 28px;
-    letter-spacing: 1px;
     color: #fff;
+    padding: 0 7px;
+    font-size: 10px;
+    letter-spacing: 0px;
+
+    ${mq("sm")} {
+      font-size: 20px;
+      padding: 0 60px;
+    }
+
+    ${mq("md")} {
+      padding: 0 100px;
+      font-size: 28px;
+      letter-spacing: 1px;
+    }
+  }
+`;
+
+const LearnMore = styled.button`
+  text-transform: capitalize;
+  background-color: #cdad7e;
+  border: 1px solid #cdad7e;
+  color: #fff;
+  border-radius: 4px;
+  letter-spacing: 1px;
+  transition: all ease-in 0.4s;
+
+  margin-left: 7px;
+  margin-top: 0px;
+  padding: 3px 7px;
+  font-size: 9px;
+
+  &:hover {
+    background: #fff;
+    color: #cdad7e;
+  }
+
+  ${mq("sm")} {
+    margin-left: 60px;
+    margin-top: 22px;
+    padding: 5px 24px;
+    font-size: 16px;
+  }
+
+  ${mq("md")} {
+    margin-left: 100px;
+    margin-top: 32px;
+    padding: 9px 56px;
+    font-size: 20px;
   }
 `;
