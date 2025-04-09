cae4d05774d6b59632a95f51eb96aec76690735d
Mohammad Akram <akramaltaf786@gmail.com>
2025-04-09 17:55:34 +0530

    Registration form completed

---
+++ src/constants/registration.ts
@@ -0,0 +1,77 @@
+export const facultyOptions: {
+  "Faculty of Quran": string[];
+  "Faculty of Alimiyat": string[];
+  "Faculty of Deeniyat": string[];
+  "Faculty of Fazilat": string[];
+  "Faculty of Languages(Arabic)": string[];
+  "Faculty of Languages(English)": string[];
+  "Faculty of Languages(Urdu)": string[];
+  "Short Courses": string[];
+} = {
+  "Faculty of Quran": [
+    "Noorani Qaedah Course",
+    "Quran Recitation Course",
+    "Tajweed Course",
+    "Quran Memorization Course",
+    "Seven Qiraats Course",
+    "Quran Ijazah Program",
+    "Quran's Translation Course",
+    "Tafseer Course",
+  ],
+  "Faculty of Deeniyat": [
+    "Deeniyat for Kids",
+    "Deeniyat for Men",
+    "Deeniyat for Women",
+  ],
+  "Faculty of Alimiyat": [
+    "Alimiyat with Darse Nizami Curriculum",
+    "Alimiyat with Modern Curriculum",
+  ],
+  "Faculty of Fazilat": [
+    "Fazilat in Quranic Science",
+    "Fazilat in Hadith Science",
+    "Fazilat in Islamic Jurisprudence",
+    "Fazilat in Theology",
+    "Fazilat in Islamic Finance",
+    "Fazilat in Arabic Language",
+  ],
+  "Faculty of Languages(Arabic)": [
+    "Foundation Arabic Course",
+    "Arabic Speaking Course",
+    "Quranic Arabic Course",
+    "Classical Arabic Course",
+    "Nah & Sarf Course",
+    "Balaghat Course",
+    "Arooz & Quafiyah Course",
+  ],
+  "Faculty of Languages(English)": [
+    "Elementary English Course",
+    "Intermediate English Course",
+    "Advanced English Course",
+  ],
+  "Faculty of Languages(Urdu)": [
+    "Elementary Urdu Course",
+    "Intermediate Urdu Course",
+    "Advanced Urdu Course",
+  ],
+  "Short Courses": [
+    "Basic Arabic",
+    "Advanced Quranic Studies",
+    "Islamic Studies for Beginners",
+  ],
+};
+
+export const timingOptions = [
+  "1:00 to 13:00",
+  "1:30 to 13:30",
+  "2:00 to 14:00",
+  "2:30 to 14:30",
+  "3:00 to 15:00",
+  "3:30 to 15:30",
+  "4:00 to 16:00",
+  "4:30 to 16:30",
+];
+
+export const languageOptions = ["Arabic", "Urdu", "English"];
+
+export const genderOptions = ["Male", "Female", "Other"];
