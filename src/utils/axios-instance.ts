7b2bcf0d252740ef1260898f6c987ca19b07b389
Mohammad Akram <akramaltaf786@gmail.com>
2025-06-15 12:47:34 +0530

    host on server

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  // baseURL: "http://localhost:5000/api", 
+  baseURL: (`${baseURL}/api`) 
 });
 
 // Add token to headers if available
